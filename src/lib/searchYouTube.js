845a67d989dbc6f9dcf269c80046f8522e1747c0
Hieu Ho <hihho@ucdavis.edu>
2020-04-06 17:58:11 -0700

    Added searchYouTube ajax functionality

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,25 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  let youtubeData = {
+    part: 'snippet',
+    maxResults: 10,
+    q: options,
+    type: 'video',
+    videoEmbeddable: true
+  };
+
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: youtubeData,
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('ajax success');
+    },
+    error: function(data) {
+      console.log('ajax error');
+    }
+  });
 };
 
 export default searchYouTube;
