ccb7bc35ce90b4423bf47aa61f741fa3dadd9da1
Hieu Ho <hihho@ucdavis.edu>
2020-04-06 20:34:13 -0700

    Attemped componentdidMount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,10 @@
-// var App = () => (
-//   <div>
-//     <nav className="navbar">
-//       <div className="col-md-6 offset-md-3">
-//         <div><h5><em>search</em> view goes here</h5></div>
-//       </div>
-//     </nav>
-//     <div className="row">
-//       <div className="col-md-7">
-//         <div><h5><em>videoPlayer</em> view goes here</h5></div>
-//       </div>
-//       <div className="col-md-5">
-//         <div><h5><em>videoList</em> view goes here</h5></div>
-//       </div>
-//     </div>
-//   </div>
-// );
-
-// // In the ES6 spec, files are "modules" and do not share a top-level scope
-// // `var` declarations will only exist globally where explicitly defined
-// export default App;
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
 
 import VideoPlayer from './VideoPlayer.js';
 import Search from './Search.js';
 import VideoList from './VideoList.js';
-
-
-
+import searchYouTube from './lib/searchYouTube.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -36,17 +15,34 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    var options = {
+      key: YOUTUBE_API_KEY,
+      query: 'react',
+      max: 5
+    };
+    searchYoutube(options, data);
+  }
+
+
 
   render() {
     return (
       <div>
-        <VideoPlayer video="{"/>
-
-        <Search />
-
-        <VideoList />
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em><Search props=""/></h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em><VideoPlayer video=""/></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em><VideoList props=""/></h5></div>
+          </div>
+        </div>
       </div>
-
     );
   }
 }
