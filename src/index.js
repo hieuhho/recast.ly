05f0d596682d5d06f9ade033e09fdf53f078b527
Hieu Ho <hihho@ucdavis.edu>
2020-04-07 15:59:33 -0700

    Import API KEY and search

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import searchYouTube from '/src/lib/searchYouTube.js';
+import YOUTUBE_API_KEY from '/src/config/youtube.js';
 
 ReactDOM.render(
-  <App />,
+  <App searchYouTube={searchYouTube} API_KEY={YOUTUBE_API_KEY}/>,
   document.getElementById('app')
 );
