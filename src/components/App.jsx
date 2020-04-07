f898a954fdb5c3ea03ec98dcc16ef2565cdec011
Hieu Ho <hihho@ucdavis.edu>
2020-04-06 23:56:10 -0700

    Added clickHandler, removed search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,42 +4,38 @@
 import VideoPlayer from './VideoPlayer.js';
 import Search from './Search.js';
 import VideoList from './VideoList.js';
-import searchYouTube from './lib/searchYouTube.js';
+// import searchYouTube from './lib/searchYouTube.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      search: '',
+      currentVideo: exampleVideoData[0],
     };
   }
 
-  componentDidMount() {
-    var options = {
-      key: YOUTUBE_API_KEY,
-      query: 'react',
-      max: 5
-    };
-    searchYoutube(options, data);
+  videoClick(video) {
+    this.setState({
+      currentVideo: video
+    });
   }
-
-
 
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em><Search props=""/></h5></div>
+            <div><h5><em>search</em><Search /></h5></div>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><h5><em>videoPlayer</em><VideoPlayer video=""/></h5></div>
+            <div><h5><em>videoPlayer</em><VideoPlayer video={this.state.currentVideo}/></h5></div>
           </div>
           <div className="col-md-5">
-            <div><h5><em>videoList</em><VideoList props=""/></h5></div>
+            <div><h5><em>videoList</em><VideoList videos={exampleVideoData} videoClick={this.videoClick.bind(this)}/></h5></div>
           </div>
         </div>
       </div>
